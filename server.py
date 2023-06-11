c43843fbc466163d51e216694a6cf65d24f3c181
NO_ob <15161159+NO-ob@users.noreply.github.com>
2023-06-11 12:35:56 +0100

    remove host ip

--- server.py
+++ server.py
@@ -179,4 +179,4 @@ def chat():
 if __name__ == '__main__':
     # run() method of Flask class runs the application
     # on the local development server.
-    app.run(debug=True, host="192.168.1.237")
+    app.run(debug=True)
